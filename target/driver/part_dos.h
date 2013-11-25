24bb7b9305fb40ffa48f5033fd68b9b94af9d870
tkmoteki <mtk3540@gmail.com>
2013-11-25 16:48:20 +0900

    Modified tab charactor & file permission

--- target/driver/part_dos.h
+++ target/driver/part_dos.h
@@ -7,26 +7,26 @@
    have 2048 byte blocks */
 #define DEFAULT_SECTOR_SIZE   2048
 #else
-#define DEFAULT_SECTOR_SIZE	512
+#define DEFAULT_SECTOR_SIZE 512
 #endif
-#define DOS_PART_TBL_OFFSET	0x1be
-#define DOS_PART_MAGIC_OFFSET	0x1fe
-#define DOS_PBR_FSTYPE_OFFSET	0x36
-#define DOS_PBR_MEDIA_TYPE_OFFSET	0x15
-#define DOS_MBR	0
-#define DOS_PBR	1
+#define DOS_PART_TBL_OFFSET 0x1be
+#define DOS_PART_MAGIC_OFFSET 0x1fe
+#define DOS_PBR_FSTYPE_OFFSET 0x36
+#define DOS_PBR_MEDIA_TYPE_OFFSET 0x15
+#define DOS_MBR 0
+#define DOS_PBR 1
 
 typedef struct dos_partition {
-	unsigned char boot_ind;		/* 0x80 - active			*/
-	unsigned char head;		/* starting head			*/
-	unsigned char sector;		/* starting sector			*/
-	unsigned char cyl;		/* starting cylinder			*/
-	unsigned char sys_ind;		/* What partition type			*/
-	unsigned char end_head;		/* end head				*/
-	unsigned char end_sector;	/* end sector				*/
-	unsigned char end_cyl;		/* end cylinder				*/
-	unsigned char start4[4];	/* starting sector counting from 0	*/
-	unsigned char size4[4];		/* nr of sectors in partition		*/
+  unsigned char boot_ind;   /* 0x80 - active      */
+  unsigned char head;   /* starting head      */
+  unsigned char sector;   /* starting sector      */
+  unsigned char cyl;    /* starting cylinder      */
+  unsigned char sys_ind;    /* What partition type      */
+  unsigned char end_head;   /* end head       */
+  unsigned char end_sector; /* end sector       */
+  unsigned char end_cyl;    /* end cylinder       */
+  unsigned char start4[4];  /* starting sector counting from 0  */
+  unsigned char size4[4];   /* nr of sectors in partition   */
 } dos_partition_t;
 
-#endif	/* _PART_DOS_H */
+#endif  /* _PART_DOS_H */
